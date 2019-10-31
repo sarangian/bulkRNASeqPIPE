35c85d8643b5497f86140b17f3f29b05741d3a13
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2019-10-31 10:49:25 +0530

    Update miniconda_x64_bulkRNASeq_install.sh

--- miniconda_x64_bulkRNASeq_install.sh
+++ miniconda_x64_bulkRNASeq_install.sh
@@ -3,21 +3,26 @@ set -e
 
 # Name of application to install
 
-echo "# Checking dependencies for bulkRNASeqPIPE installation ..."
-sleep 2s;
+echo ""
+echo -e "\e[1;34mChecking dependencies for bulkRNASeqPIPE installation ...\e[0m"
+echo ""
 
-declare -a dpkglist=("libssl-dev" "libcurl4-openssl-dev" "libxml2-dev" "libboost-all-dev" "libbz2-dev" "liblzma-dev")
-for package in "${dpkglist[@]}";
-	do
-  		if [ $(dpkg-query -W -f='${Status}' $package 2>/dev/null | grep -c "ok installed") -eq 1 ];
-			then
-  			echo -e "\e[1;36m $package \t...installed \e[0m";
-		else
-  			echo -e "\e[1;31m install $package manually using \"sudo apt-get install package-name\" The installer will auto exit now\e[0m";
-			sleep 5s;
-			exit 0
-		fi
-	done
+sleep 2s;
+#Check Ubuntu
+if [ -f /etc/lsb-release ]; then
+    declare -a dpkglist=("libssl-dev" "libcurl4-openssl-dev" "libxml2-dev" "libboost-all-dev" "libbz2-dev" "liblzma-dev")
+    for package in "${dpkglist[@]}";
+	    do
+  		    if [ $(dpkg-query -W -f='${Status}' $package 2>/dev/null | grep -c "ok installed") -eq 1 ];
+			    then
+  			    echo -e "\e[1;36m $package \t...installed \e[0m";
+		    else
+  			    echo -e "\e[1;31m install $package manually using \"sudo apt-get install package-name\" The installer will auto exit now\e[0m";
+			    sleep 5s;
+			    exit 0
+		    fi
+	    done
+fi
 
 #Packages through git, wget and Sourseforge
 
