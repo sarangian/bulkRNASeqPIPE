06fe567e7b3c369f270e088fe20917d58f58e0c0
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2019-12-09 10:15:23 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,41 @@
 set -e
 
 # Name of application to install
+echo ""
+echo -e "\e[1;34mChecking dependencies for bulkRNASeqPIPE installation ...\e[0m"
+echo ""
+
+sleep 2s;
+#Check Ubuntu
+if [ -f /etc/lsb-release ]; then
+    declare -a dpkglist=("zlib1g-dev")
+    for package in "${dpkglist[@]}";
+	    do
+  		    if [ $(dpkg-query -W -f='${Status}' $package 2>/dev/null | grep -c "ok installed") -eq 1 ];
+			    then
+  			    echo -e "\e[1;36m $package \t...installed \e[0m";
+		    else
+  			    echo -e "\e[1;31m install $package manually using \"sudo apt-get install $package\" \nThe installer will auto exit now\e[0m";
+			    sleep 5s;
+			    exit 0
+		    fi
+	    done
+fi
+
+if [ -f /etc/redhat-release ]; then
+    declare -a dpkglist=("zlib-devel")
+    for package in "${dpkglist[@]}";
+	    do
+  		    if rpm -qa | grep $package;
+			    then
+  			    echo -e "\e[1;36m $package \t...installed \e[0m";
+		    else
+  			    echo -e "\e[1;31m install $package manually using \"sudo yum install $package\" \nThe installer will auto exit now\e[0m";
+			    sleep 5s;
+			    exit 0
+		    fi
+	    done
+fi
 
 #Packages through git, wget and Sourseforge
 
