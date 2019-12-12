53fea36ce61087ae776186815f013589c3e620a0
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2019-12-12 13:20:03 +0530

    Update and rename run_arthrinium.sh to .run_arthrinium.sh

--- .run_arthrinium.sh
+++ .run_arthrinium.sh
@@ -8,4 +8,4 @@
 #python bulkRNASeqWF.py transcriptomeBasedDEA --projectName arthrinium --domain eukaryote --predTranscript no --readType paired --quantMethod salmon --genomeName arthrinium --transcriptName arthrinium --deaMethod deseq2 --factorInt group --refCond control --resultTag arthrinium_treated_vs_control --targetFile arthrinium_target.txt --local-scheduler 2>&1 | tee arthrinium_transcriptome_salmon_based_DEA_DESeq2.log
 
 #Genome based differential expression analysis
-python bulkRNASeqWF.py genomeBasedDEA --projectName arthrinium --domain eukaryote --aligner hisat2 --readType paired --genomeName arthrinium --deaMethod deseq2 --resultTag arthrinium_treated_vs_control --factorInt group --refCond control --targetFile arthrinium_target.txt --local-scheduler 2>&1 | tee arthrinium_genome_based_DEA_DESeq2.log
+#python bulkRNASeqWF.py genomeBasedDEA --projectName arthrinium --domain eukaryote --aligner hisat2 --readType paired --genomeName arthrinium --deaMethod deseq2 --resultTag arthrinium_treated_vs_control --factorInt group --refCond control --targetFile arthrinium_target.txt --local-scheduler 2>&1 | tee arthrinium_genome_based_DEA_DESeq2.log
