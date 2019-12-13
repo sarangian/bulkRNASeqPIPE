8c98e757b5b07413a38c5e531b7568e5d8dd22ae
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2019-12-13 14:59:38 +0530

    Update run_arthrinium.sh

--- run_arthrinium.sh
+++ run_arthrinium.sh
@@ -3,9 +3,6 @@ gunzip $(pwd)/raw_data/genome/arthrinium/arthrinium.fna.gz
 ##Prepare Project
 python bulkRNASeqWF.py prepareProject --projectName arthrinium --sampleMetadataFile arthrinium_target.txt --sampleHeader samples --groupHeader group --refCondName control --local-scheduler
 
-#denovo Transcriptome Assembly
-#python RNASeqPIPE.py denovoTransAssemble --projectName arthrinium --domain eukaryote --readType paired --assembler trinity --local-scheduler
-
 #Denovo Transcriptome Assembly and differential expression analysis
 #python RNASeqPIPE.py denovoDEA --projectName arthrinium --assembler trinity --deaMethod deseq2 --resultTag treated_vs_control --readType paired --reportName denovo_trinity_DESeq2 --factorInt group --refCond control --domain eukaryote --targetFile arthrinium_target.txt --local-scheduler 2>&1 | tee arthrinium_deovo_DEA_trinity.log
 
@@ -17,3 +14,6 @@ python bulkRNASeqWF.py prepareProject --projectName arthrinium --sampleMetadataF
 
 #Genome based differential expression analysis
 #python RNASeqPIPE.py genomeBasedDEA --projectName arthrinium --domain eukaryote --aligner hisat2 --readType paired --genomeName arthrinium --deaMethod deseq2 --resultTag arthrinium_treated_vs_control --factorInt group --refCond control --targetFile arthrinium_target.txt --local-scheduler 2>&1 | tee arthrinium_genome_based_DEA_DESeq2.log
+
+#denovo Transcriptome Assembly
+#python RNASeqPIPE.py denovoTransAssemble --projectName arthrinium --domain eukaryote --readType paired --assembler trinity --local-scheduler
